3ec01875c4be94e111d02adbf5bdcfa0bdd8f6ef
johanstokking <johan@stokking.nl>
2016-04-05 09:44:54 -0700

    Added sensor sample

---
+++ workshops/mdot/samples/sensors.cpp
@@ -0,0 +1,11 @@
+#include "mbed.h"
+
+float readTemperature(AnalogIn pin) {
+    float value = pin.read();
+    float celcius = (value * 3.3f - 0.6f) * 100.0f;
+    float fahrenheit = celcius * 1.8f / 32;
+    
+    //return celcius;
+    
+    return fahrenheit;
+}
