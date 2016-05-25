cb90f27bfc4e46afe02ebaa40a42e6a683814f23
johanstokking <johan@stokking.nl>
2016-05-25 17:52:46 +0200

    Updated NPM repository

--- integrations/aws/examples/simple.js
+++ integrations/aws/examples/simple.js
@@ -1,35 +1,15 @@
-# The Things Network Example Integration with AWS IoT
-
-This is an example integration of The Things Network with AWS IoT.
-
-[Learn more about AWS IoT](https://aws.amazon.com/iot/how-it-works/)
-
-## Setup
-
-Make sure you have your device(s) registered in AWS IoT. The ID of your device must be your DevEUI
-
-# Install
-
-Install the NPM packages:
-```
-npm install ttn-aws-iot
-```
-
-# Example
-
-```js
 'use strict';
 
-const ttnawsiot = require('ttn-aws-iot');
+const ttnawsiot = require('..');
 
 // Replace with your AppEUI and App Access Key
 const appEUI = '<insert AppEUI>';
 const appAccessKey = '<insert App Access Key>';
 
 // Replace with your certificates from AWS IoT
-const caCert = 'examples/keys/root-CA.crt';
-const clientCert = 'examples/keys/cert.crt';
-const privateKey = 'examples/keys/private.key';
+const caCert = 'keys/root-CA.crt';
+const clientCert = 'keys/cert.crt';
+const privateKey = 'keys/private.key';
 const region = 'eu-west-1';
 
 const bridge = new ttnawsiot.Bridge(appEUI, appAccessKey, caCert, clientCert, privateKey, region);
@@ -49,4 +29,3 @@ bridge.on('error', err => {
 bridge.on('uplink', data => {
   console.log('Uplink', data);
 });
-```
--- integrations/aws/index.js
+++ integrations/aws/index.js
@@ -3,66 +3,43 @@
 const util = require('util');
 const ttn = require('ttn');
 const awsiot = require('aws-iot-device-sdk');
+const EventEmitter = require('events');
 
-const mqttBroker = 'staging.thethingsnetwork.org'
-const appEUI = '<insert your AppEUI>';
-const appAccessKey = '<insert your App Access Key>';
-const keyPath = './devices';
-const region = 'eu-west-1';
+const defaultTTNBroker = 'staging.thethingsnetwork.org';
 
-// Create the client to The Things Network
-var client = new ttn.Client(mqttBroker, appEUI, appAccessKey);
+const Bridge = class Bridge extends EventEmitter {
+  constructor(appEUI, appAccessKey, caCert, clientCert, privateKey, region, options) {
+    super();
+    options = options || {};
 
-var devices = {};
+    this.awsClient = awsiot.device({
+      caCert: caCert,
+      clientCert: clientCert,
+      privateKey: privateKey,
+      clientId: options.clientId || 'ttn-bridge',
+      region: region
+    });
+    this.awsClient.on('connect', super.emit.bind(this, 'aws-connect'));
+    this.awsClient.on('error', super.emit.bind(this, 'error'));
 
-// A device activates
-client.on('activation', activation => {
-  console.log('%s: Device activated', activation.devEUI);
-
-  // If we already have a connection, close it first
-  if (devices[activation.devEUI]) {
-    console.log('%s: Closing existing connection', activation.devEUI);
-    devices[activation.devEUI].end();
+    this.ttnClient = new ttn.Client(options.ttnBroker || defaultTTNBroker, appEUI, appAccessKey);
+    this.ttnClient.on('connect', super.emit.bind(this, 'ttn-connect'));
+    this.ttnClient.on('error', super.emit.bind(this, 'error'));
+    this.ttnClient.on('uplink', this._handleUplink.bind(this));
   }
 
-  // Initialize the device with the security keys. Get these from the AWS IoT
-  var device = awsiot.device({
-    clientCert: util.format('%s/%s.crt', keyPath, activation.devEUI),
-    privateKey: util.format('%s/%s.key', keyPath, activation.devEUI),
-    caCert: util.format('%s/root-CA.crt', keyPath),
-    clientId: activation.devEUI,
-    region: region
-  });
+  _handleUplink(uplink) {
+    var data = {
+      state: {
+        reported: uplink.fields
+      }
+    };
 
-  device.on('connect', () => {
-    console.log('%s: Connected', activation.devEUI);
-    devices[activation.devEUI] = device;
-  });
-
-  device.on('error', err => {
-    console.log('%s: %s', err);
-  })
-});
-
-// A device sends a message
-client.on('uplink', uplink => {
-  console.log('%s: Got uplink: %j', uplink.devEUI, uplink.fields);
-
-  var device = devices[uplink.devEUI];
-  if (!device) {
-    console.warn('%s: Device is not activated', uplink.devEUI);
-    return;
+    super.emit('uplink', data);
+    this.awsClient.publish(util.format('$aws/things/%s/shadow/update', uplink.devEUI), JSON.stringify(data));
   }
+}
 
-  // Prepare the message to send
-  var data = Object.assign({
-    counter: uplink.counter,
-    time: uplink.metadata.server_time
-  }, uplink.fields);
-
-  // Send the message to AWS IoT
-  console.log('%s: Sending message: %j', uplink.devEUI, data);
-  device.publish('up', JSON.stringify(data));
-});
-
-client.connect();
+module.exports = {
+  Bridge: Bridge
+};
